85a15f51d4226b431c6ce43fa79c3a3042e7da48
Ilhee <asahi8769@gmail.com>
2020-07-06 14:56:56 +0900

    Add all my files

--- git_.py
+++ git_.py
@@ -9,14 +9,14 @@ class GitCommandLines():
         subprocess_cmd(f'git config --global user.email asahi8769@gmail.com')
 
     def push_rep(self):
-
+        self.clone_rep()
         subprocess_cmd (f'git init')
         subprocess_cmd (f'git add .')
         subprocess_cmd (f'git config --global http.sslVerify false')
-        # subprocess_cmd (f'git rm -r --cached images.zip')
+        subprocess_cmd (f'git rm -r --cached old/')
         # subprocess_cmd (f'git rm -r --cached .idea/')
         # subprocess_cmd (f'git rm -r --cached __pycache__/')
-        self.clone_rep()
+
         subprocess_cmd (f'git commit -m "Add all my files"')
         subprocess_cmd (f'git remote add origin {self.repository}')
         subprocess_cmd (f'git push --force origin master')
