cd53c39c275d42625f998a6df837243c968d2cb0
Ilhee <asahi8769@gmail.com>
2020-07-06 09:44:22 +0900

    Add all my files

--- venv_const.py
+++ venv_const.py
@@ -5,5 +5,4 @@ dir_venv_64 = r'D:\devs\Objections\venv\Scripts'
 def install(lib):
     return f'pip --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org install {lib}'
 
-
 subprocess_cmd(f'cd {dir_venv_64}  & {install("pyinstaller")} & {install("xlrd")} & {install("selenium")} & {install("pandas")} & {install("pyautogui")}')
