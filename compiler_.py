afb0a32201fe8eff085e8b4145bcc89667e3fb94
Ilhee <asahi8769@gmail.com>
2020-08-20 15:46:29 +0900

    Apply all changes

--- compiler_.py
+++ compiler_.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     file_to_compile = file_to_compile.replace('\\', '/')
     icon_name = os.path.join(os.getcwd(), 'Cyberduck.ico').replace('\\', '/')
 
-    install_command = f'pyinstaller.exe -F --hidden-import=xlrd --icon={icon_name} {file_to_compile}'
+    install_command = f'pyinstaller.exe -F --hidden-import=xlrd  {file_to_compile}'
     dir_loc = os.path.join(os.getcwd(), 'dist')
     print(install_command)
     subprocess_cmd(f'cd {dir_venv_64} & {install_command} & cd dist & copy {file_name_exe} {dir_loc}')
