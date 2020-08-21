c356b913ab9cbef8ba3bf4f55bf87aa90fa85b23
Ilhee <asahi8769@gmail.com>
2020-08-21 14:29:08 +0900

    Apply all changes

--- compiler_.py
+++ compiler_.py
@@ -1,9 +1,11 @@
 from subprocess import Popen, PIPE
 import os, sys
 from utility_functions import *
-sys.path.insert(1, os.pardir)
+
+# sys.path.insert(1, os.pardir)
 
 if __name__ == "__main__":
+
     make_dir(os.path.join(os.getcwd(), 'dist'))
 
     dir_venv_64 = os.path.join(os.getcwd(), 'venv', 'Scripts')
@@ -11,11 +13,12 @@ if __name__ == "__main__":
     file_name_exe = 'Entry.exe'
     file_to_compile = path_find(file_name_py, os.getcwd())
     file_to_compile = file_to_compile.replace('\\', '/')
-    icon_name = os.path.join(os.getcwd(), 'Cyberduck.ico').replace('\\', '/')
+    icon_name = path_find('Ph03nyx-Super-Mario-Mushroom-1UP.ico', os.getcwd()).replace('\\', '/')
 
-    install_command = f'pyinstaller.exe -F --hidden-import=xlrd  {file_to_compile}'
+    # install_command = f'pyinstaller.exe --onefile --hidden-import=xlrd --icon={icon_name} {file_to_compile}'
+    install_command = f'pyinstaller.exe --onefile --hidden-import=xlrd {file_to_compile}'
     dir_loc = os.path.join(os.getcwd(), 'dist')
     print(install_command)
     subprocess_cmd(f'cd {dir_venv_64} & {install_command} & cd dist & copy {file_name_exe} {dir_loc}')
     packaging(file_name_exe, 'Cookies_objection', 'driver')
-    os.startfile('dist')
+    # os.startfile('dist')
