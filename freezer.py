7f2c2ee5bd6757872962d8c09784f672e0c836c9
Ilhee <asahi8769@gmail.com>
2020-09-24 15:53:42 +0900

    20200924_15

--- freezer.py
+++ freezer.py
@@ -4,16 +4,19 @@ from utils.config import SCRIPTS_DIR
 
 if __name__ == "__main__":
     make_dir(os.path.join(os.getcwd(), 'dist'))
-    # dir_venv_64 = os.path.join(os.getcwd(), 'venv', 'Scripts')
+
     file_name_py = 'Entry.py'
     file_name_exe = 'Entry.exe'
     file_to_compile = path_find(file_name_py, os.getcwd())
     file_to_compile = file_to_compile.replace('\\', '/')
     icon_name = "Cyberduck.ico"
+
     freeze_command_with_icon = f'pyinstaller.exe --onefile --hidden-import=xlrd --icon={icon_name} {file_to_compile}'
     freeze_command_without_icon = f'pyinstaller.exe --onefile --hidden-import=xlrd {file_to_compile}'
     dist_dir = os.path.join(os.getcwd(), 'dist')
+
     subprocess_cmd(f'cd {SCRIPTS_DIR} & {freeze_command_without_icon} & cd dist & '
                    f'copy {file_name_exe} {dist_dir} & copy {file_name_exe} {os.getcwd()}')
+
     packaging(file_name_exe, 'Cookies_objection', 'driver')
     os.startfile('dist')
