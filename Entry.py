6814bfd8a6e211db588dda9ac71ea9876295d283
Ilhee <asahi8769@gmail.com>
2020-10-05 09:35:22 +0900

    20201005_09

--- Entry.py
+++ Entry.py
@@ -12,12 +12,10 @@ from selenium.webdriver.common.keys import Keys
 import warnings, time, operator, gc, sys, os, pyautogui, pyperclip
 import pandas as pd
 
-
-warnings.filterwarnings ('ignore')
+warnings.filterwarnings('ignore')
 
 
 class CustomerObjection:
-
     """Current chromedriver version is 83. In order to keep this driver compatible with running chromebrowser version,
     you can download newer version of chromedriver or disable chromebrowser's auto update function.
     If chromebrowser version is ahead of the installed chromedriver version, chromedriver cannot manipulate chromebrowser.
@@ -32,6 +30,7 @@ class CustomerObjection:
     GC_DRIVER = 'driver/chromedriver.exe'
     CHROME_OPTIONS = Chrome_options()
     CHROME_OPTIONS.add_argument("--start-maximized")
+
     # CHROME_OPTIONS.add_argument("--disable-extensions")
     # CHROME_OPTIONS.add_argument("--incognito")
 
@@ -42,6 +41,7 @@ class CustomerObjection:
         self.tot_seq = len(objections.storage)
         self.objset = objections.objection_generator()
         self.delimiter = objections.delimiter
+        self.filters = objections.filters
         self.customer = None
         self.driver = webdriver.Chrome(self.GC_DRIVER, options=self.CHROME_OPTIONS)
         self.driver.get(URL)
@@ -52,8 +52,8 @@ class CustomerObjection:
 
     def click_element_id(self, ID, sec):
         try:
-            element = WebDriverWait (self.driver, sec).until(
-                EC.element_to_be_clickable ((By.ID, ID)))
+            element = WebDriverWait(self.driver, sec).until(
+                EC.element_to_be_clickable((By.ID, ID)))
             element.click()
             return element
         except TimeoutException:
@@ -64,25 +64,26 @@ class CustomerObjection:
         self.click_element_id('mainframe_VFrameSet_LoginFrame_CommLgds010_3_form_ImageViewer00', 1)
         self.click_element_id('mainframe_VFrameSet_LoginFrame_CommLgds010_2_form_ImageViewer00', 1)
         self.click_element_id('mainframe_VFrameSet_LoginFrame_CommLgds010_1_form_ImageViewer00', 1)
-        self.click_element_id ('mainframe_VFrameSet_LoginFrame_CommLgds010_0_form_ImageViewer00', 1)
+        self.click_element_id('mainframe_VFrameSet_LoginFrame_CommLgds010_0_form_ImageViewer00', 1)
         id_box = self.driver.find_element_by_id('mainframe_VFrameSet_LoginFrame_form_div_logo_edt_userId_input')
         login_button = self.driver.find_element_by_id('mainframe_VFrameSet_LoginFrame_form_div_logo_btn_login')
         id_box.send_keys(GSCM_ID)
-        ActionChains (self.driver).send_keys (Keys.TAB).send_keys (GSCM_PW).perform ()
-        login_button.click ()
+        ActionChains(self.driver).send_keys(Keys.TAB).send_keys(GSCM_PW).perform()
+        login_button.click()
 
     def inner_remove_noti(self):
-        self.click_element_id ('mainframe_VFrameSet_TopFrame_CommLgds010P_4_form_ImageViewer00', 3)  # todo : consider commenting out
-        self.click_element_id ('mainframe_VFrameSet_TopFrame_CommLgds010P_3_form_ImageViewer00', 1)
-        self.click_element_id ('mainframe_VFrameSet_TopFrame_CommLgds010P_2_form_ImageViewer00', 1)
-        self.click_element_id ('mainframe_VFrameSet_TopFrame_CommLgds010P_1_form_ImageViewer00', 1)
-        self.click_element_id ('mainframe_VFrameSet_TopFrame_CommLgds010P_0_form_ImageViewer00', 1)
-        self.click_element_id ('mainframe_VFrameSet_TopFrame_gfn_alert_form_alert_bottom_bg_btn_confirm', 1)
+        self.click_element_id('mainframe_VFrameSet_TopFrame_CommLgds010P_4_form_ImageViewer00',
+                              3)  # todo : consider commenting out
+        self.click_element_id('mainframe_VFrameSet_TopFrame_CommLgds010P_3_form_ImageViewer00', 1)
+        self.click_element_id('mainframe_VFrameSet_TopFrame_CommLgds010P_2_form_ImageViewer00', 1)
+        self.click_element_id('mainframe_VFrameSet_TopFrame_CommLgds010P_1_form_ImageViewer00', 1)
+        self.click_element_id('mainframe_VFrameSet_TopFrame_CommLgds010P_0_form_ImageViewer00', 1)
+        self.click_element_id('mainframe_VFrameSet_TopFrame_gfn_alert_form_alert_bottom_bg_btn_confirm', 1)
 
     def export_page(self):
         self.log_in()
         self.inner_remove_noti()
-        dropdown = self.click_element_id ('mainframe_VFrameSet_TopFrame_form_cbo_auth_sub_dropbutton', 5)
+        dropdown = self.click_element_id('mainframe_VFrameSet_TopFrame_form_cbo_auth_sub_dropbutton', 5)
         dropdown.send_keys(Keys.DOWN)
         dropdown.send_keys(Keys.RETURN)
 
@@ -99,7 +100,7 @@ class CustomerObjection:
         """Redesigned on 2020.03.30"""
         seq_m = self.click_element_id('K_RPYM_WNTI_MM', 3)
         seq_m.send_keys(Keys.DELETE)
-        seq_y = self.click_element_id('K_RPYM_WNTI_YY',3)
+        seq_y = self.click_element_id('K_RPYM_WNTI_YY', 3)
         seq_y.send_keys(issuemonth[0:4])
         seq_m.send_keys(issuemonth[4:6])
         time.sleep(wait)
@@ -107,7 +108,7 @@ class CustomerObjection:
     def counter(self, num):
         self.sequence += 1
         if self.stop:
-            if self.sequence == num+1:
+            if self.sequence == num + 1:
                 self.close()
                 print('TERMINATED BY SCRIPT')
                 sys.exit()
@@ -127,14 +128,15 @@ class CustomerObjection:
                         By.XPATH, "//select[@name='K_RPYM_WNTI_NO']/option[@value='{}']".format(
                             feed[5][0] + feed[5][1] + '-' + feed[5][2] + '-' + feed[1][0])))).click()
                 break
-            except :
+            except:
                 print('TIME OUT')
                 continue
 
     def creation_loop(self, feed):
         """Redesigned on 2020.03.25"""
-        index = [feed[0] + self.delimiter +feed[1]+ self.delimiter+feed[2]+ self.delimiter +str(feed[3])+ self.delimiter +feed[4]]
-        if 'Created' in self.df.loc[index]['Result'].tolist() :
+        index = [feed[0] + self.delimiter + feed[1] + self.delimiter + feed[2] + self.delimiter + str(
+            feed[3]) + self.delimiter + feed[4]]
+        if 'Created' in self.df.loc[index]['Result'].tolist():
             return
         noc = 1
         self.setting(feed)
@@ -145,8 +147,8 @@ class CustomerObjection:
               '\nLength : {}'.format(len(feed[6])), '\nAmount : {}'.format(feed[-1]), '\nIssueMonth : {}'.format(
                 feed[5][0] + feed[5][1] + '-' + feed[5][2] + '-' + feed[1][0]))
         for n in range(len(feed[6])):
-            if not chained :
-                front = self.click_element_id ('K_FIRM_INFM_SN_F', 3)
+            if not chained:
+                front = self.click_element_id('K_FIRM_INFM_SN_F', 3)
                 front.send_keys(Keys.DELETE)
                 front.send_keys(str(feed[6][n]))
             if n != len(feed[6]) - 1 and feed[6][n] - feed[6][n + 1] == -1 and noc < 500:
@@ -154,7 +156,7 @@ class CustomerObjection:
                 chained = True
                 print(feed[6][n], end='=>')
                 continue
-            else :
+            else:
                 noc = 1
                 chained = False
                 tail = self.click_element_id('K_FIRM_INFM_SN_T', 3)
@@ -164,19 +166,19 @@ class CustomerObjection:
                 self.click_element_id('searchBtn', 3)
                 time.sleep(0.7)
                 self.click_element_id('groupRejectBtn', 3)
+                self.update_hist(feed, 'Created')
                 continue
         self.logging(feed, 'Created')
-        self.update_hist(feed, 'Created')
 
     def register(self, feed):
         WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable(
             (By.XPATH, '//*[@id="menu"]/div[3]/ul/li[4]/a'))).click()
         self.setting(feed)
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((
-                By.XPATH, "//*[@id='K_GUBN']/option[@value='N']"))).click()
+            By.XPATH, "//*[@id='K_GUBN']/option[@value='N']"))).click()
         time.sleep(1)
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((
-                By.XPATH, "//*[@id='K_MKOB_TYPE_CD']/option[@value='{}']".format(feed[4])))).click()
+            By.XPATH, "//*[@id='K_MKOB_TYPE_CD']/option[@value='{}']".format(feed[4])))).click()
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((
             By.XPATH, '//*[@id="searchBtn"]'))).click()
         pyperclip.copy(feed[3])
@@ -196,7 +198,7 @@ class CustomerObjection:
             time.sleep(0.7)
             pyautogui.press('enter')
             time.sleep(0.5)
-            letter_box_1 = self.click_element_id ('D_MKOB_TITL_NM', 5)
+            letter_box_1 = self.click_element_id('D_MKOB_TITL_NM', 5)
             letter_box_1.send_keys(str(feed[3]))
             letter_box_2 = self.click_element_id('D_MKOB_DTL_SBC', 5)
             letter_box_2.send_keys(str(feed[3]))
@@ -204,7 +206,7 @@ class CustomerObjection:
             time.sleep(0.7)
             pyautogui.press('enter')
             time.sleep(0.5)
-            self.logging (feed, 'Registered')
+            self.logging(feed, 'Registered')
             self.update_hist(feed, 'Registered')
         else:
             self.driver.switch_to.window(self.driver.window_handles[1])
@@ -215,17 +217,18 @@ class CustomerObjection:
         """Redesigned on 2020.03.30"""
         WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable(
             (By.XPATH, '//*[@id="menu"]/div[3]/ul/li[5]/a'))).click()
-        print ('\nCustomer : {}, Starting month : {}, Length : {}, Amount : {}'.format(self.customer, min(min_year), self.length, self.amount))
+        print('\nCustomer : {}, Starting month : {}, Length : {}, Amount : {}'.format(self.customer, min(min_year),
+                                                                                      self.length, self.amount))
         self.input_year_month(str(min(min_year)), 1)
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((
             By.XPATH, "//select[@name='K_PRDN_CORP_CD']/option[text()='{}']".format(self.customer)))).click()
         self.click_element_id('searchBtn', 3)
 
     def mainloop(self):
-        now = datetime.now ()
-        dt_string = now.strftime ("%Y/%m/%d")
-        with open ('Cookies_objection/log.txt', 'a+') as txt:
-            txt.write (f'\n{dt_string} Initiated\n')
+        now = datetime.now()
+        dt_string = now.strftime("%Y/%m/%d")
+        with open('Cookies_objection/log.txt', 'a+') as txt:
+            txt.write(f'\n{dt_string} Initiated\n')
         start = time.time()
         self.get_workfloor()
         WebDriverWait(self.driver, 7).until(EC.element_to_be_clickable(
@@ -233,12 +236,12 @@ class CustomerObjection:
         min_year = list()
         for feed in self.objset:
             self.customer = feed[0].upper()
-            min_year.append (int (feed[5][0] + feed[5][1]))
+            min_year.append(int(feed[5][0] + feed[5][1]))
             self.counter(3)
             self.creation_loop(feed)
             self.register(feed)
         end = time.time()
-        elapsed = 'Elapsed {0:02d}:{1:02d}'.format(*divmod(int(end-start), 60))
+        elapsed = 'Elapsed {0:02d}:{1:02d}'.format(*divmod(int(end - start), 60))
         print(elapsed)
         now = datetime.now()
         dt_string = now.strftime("%Y/%m/%d")
@@ -250,22 +253,28 @@ class CustomerObjection:
                  f'\n소요시간 : {elapsed} \n금액, 건수 검증하고 이의제기 의뢰하세요. \n의뢰 한 후 브라우저를 닫으세요.',
             title='프로세스종료알림', button='OK')
         self.save_df()
-        input ('Press <ENTER> to terminate...')
+        input('Press <ENTER> to terminate...')
         self.close()
 
     def logging(self, feed, stage):
         now = datetime.now()
         dt_string = now.strftime("%Y/%m/%d %H:%M:%S")
         list_no = ', '.join([str(i) for i in feed[6]])
-        self.log = dt_string+' '+ feed[0]+' '+ feed[5][3]+'-'+ feed[1][0]+', '+ feed[3]+f', ({list_no})' +f', {stage}\n'
+        self.log = dt_string + ' ' + feed[0] + ' ' + feed[5][3] + '-' + feed[1][0] + ', ' + feed[
+            3] + f', ({list_no})' + f', {stage}\n'
         with open('Cookies_objection/log.txt', 'a+') as txt:
             txt.write(self.log)
 
     def update_hist(self, feed, stage):
-        self.df.at[feed[0] + self.delimiter +feed[1]+ self.delimiter+feed[2]+ self.delimiter +str(feed[3])+ self.delimiter +feed[4], 'Result'] = stage
+        print('Updating ...')
+        # print(len(self.df.at[feed[0] + self.delimiter + feed[1] + self.delimiter + feed[2] + self.delimiter + feed[3] +
+        #            self.delimiter + feed[4], 'Result']))
+        self.df.at[feed[0] + self.delimiter + feed[1] + self.delimiter + feed[2] + self.delimiter + feed[3] +
+                   self.delimiter + feed[4], 'Result'] = stage
+
 
     def save_df(self):
-        self.df['Result'] = np.where(self.df['Customer Reivew_'] != '', '', self.df['Result'])
+        self.df['Result'] = np.where(self.filters, self.df['Result'], '')
         self.df.to_excel(r'Cookies_objection\objection.xls', index=False)
         os.startfile(r'Cookies_objection\objection.xls')
 
@@ -297,13 +306,13 @@ class CustomerObjection:
 
     def close(self):
         try:
-            self.driver.delete_all_cookies ()
-            self.driver.quit ()
+            self.driver.delete_all_cookies()
+            self.driver.quit()
         except Exception as e:
             pass
 
     def __del__(self):
-        try :
+        try:
             self.driver.delete_all_cookies()
         except Exception as e:
             print('Screen terminated according to nominal procedure.')
@@ -314,33 +323,32 @@ class CustomerObjection:
 class Pipeline:
     def __init__(self):
         self.filename = r'Cookies_objection\objection.xls'
-        with open (self.filename, 'rb') as file:
-            self.df = pd.read_excel (file)
+        with open(self.filename, 'rb') as file:
+            self.df = pd.read_excel(file)
             self.df.fillna('', inplace=True)
 
         self.customer = self.df.iloc[0]['고객사']
         self.delimiter = ',,'
-        self.df['KEY'] = self.df['고객사'] + self.delimiter + self.df['E/D'] + self.delimiter + self.df['ISSUE NO'] +\
+        self.df['KEY'] = self.df['고객사'] + self.delimiter + self.df['E/D'] + self.delimiter + self.df['ISSUE NO'] + \
                          self.delimiter + self.df['OBJECTION_'].apply(str) + self.delimiter + self.df['유형']
         self.df.set_index('KEY', inplace=True)
         if 'Result' not in self.df.columns:
             self.df['Result'] = ['' for _ in range(len(self.df))]
-        self.df_ = self.df[~(self.df['Customer Reivew_'].str.contains('reject')) &
-                          ~(self.df['Customer Reivew_'].str.contains('wait')) &
-                          ~(self.df['Customer Reivew_'].str.contains('pending')) &
-                          ~(self.df['Customer Reivew_'].str.contains('denied')) &
-                          ~(self.df['Customer Reivew_'].str.contains('wrong')) &
-                          ~(self.df['Customer Reivew_'].str.contains('deny')) &
-                          ~(self.df['Customer Reivew_'].str.contains('done')) &
-                          ~(self.df['Customer Reivew_'].str.contains('cancel')) &
-                          ~(self.df['Customer Reivew_'].str.contains('기각')) &
-                          ~(self.df['Result'].str.contains('Registered'))
-        ]
-
-        self.keys = set (self.df_.index)
+        self.filters = (~(self.df['Customer Reivew_'].str.contains('reject')) &
+                         ~(self.df['Customer Reivew_'].str.contains('wait')) &
+                         ~(self.df['Customer Reivew_'].str.contains('pending')) &
+                         ~(self.df['Customer Reivew_'].str.contains('denied')) &
+                         ~(self.df['Customer Reivew_'].str.contains('wrong')) &
+                         ~(self.df['Customer Reivew_'].str.contains('deny')) &
+                         ~(self.df['Customer Reivew_'].str.contains('done')) &
+                         ~(self.df['Customer Reivew_'].str.contains('cancel')) &
+                         ~(self.df['Customer Reivew_'].str.contains('기각')) &
+                         ~(self.df['Result'].str.contains('Registered')))
+        self.df_ = self.df[self.filters]
+        self.keys = set(self.df_.index)
         self.storage = list()
         self.data = self.isolate()
-        try :
+        try:
             self.print_example()
         except IndexError as e:
             print(f'No data is available. {e}')
@@ -349,26 +357,26 @@ class Pipeline:
 
     def isolate(self):
         for key in self.keys:
-            val_list = key.split (self.delimiter)
-            val_list.append ([val_list[2][0:4], val_list[2][4:6], val_list[2][6:],
-                              val_list[2][0:4] + '-' + val_list[2][4:6] + '-' + val_list[2][6:]])
+            val_list = key.split(self.delimiter)
+            val_list.append([val_list[2][0:4], val_list[2][4:6], val_list[2][6:],
+                             val_list[2][0:4] + '-' + val_list[2][4:6] + '-' + val_list[2][6:]])
             partial_df = self.df_.loc[key]
             try:
-                val_list.append (sorted (partial_df['LIST'].to_numpy ().tolist ()))
+                val_list.append(sorted(partial_df['LIST'].to_numpy().tolist()))
             except AttributeError:
-                val_list.append ([partial_df['LIST']])
-            val_list.append (round (partial_df['전체'].sum (), 2))
+                val_list.append([partial_df['LIST']])
+            val_list.append(round(partial_df['전체'].sum(), 2))
             self.storage.append(val_list)
-        self.storage = sorted(self.storage, key = operator.itemgetter(0,1,2,3,6))
+        self.storage = sorted(self.storage, key=operator.itemgetter(0, 1, 2, 3, 6))
         return self.storage
 
     def print_example(self):
-        example = list(enumerate (self.storage))[0][1]
+        example = list(enumerate(self.storage))[0][1]
         print('\nNumber of Objections : {}, Total Amount : {:,.2f}, Total Sequence : {}'.format(
-            len(self.df_), round(self.df_['전체'].sum(),2), len(self.storage)))
-        print ('\nFeed Format : {}'.format(example))
-        for i, item in enumerate (example):
-            print ('Index {} : {}'.format (i, item))
+            len(self.df_), round(self.df_['전체'].sum(), 2), len(self.storage)))
+        print('\nFeed Format : {}'.format(example))
+        for i, item in enumerate(example):
+            print('Index {} : {}'.format(i, item))
 
     def objection_generator(self):
         for datum in self.data:
@@ -377,4 +385,3 @@ class Pipeline:
 
 if __name__ == '__main__':
     CustomerObjection.run()
-
